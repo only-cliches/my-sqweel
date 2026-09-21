b5dcdf5a6096cfa4a60bcbe449a2120afb2b4608
Scott Lott <me@scottlott.com>
2026-09-21 16:02:02 +0000

    Fix JSON_MERGE metadata parity

--- src/sql/engine/query.rs
+++ src/sql/engine/query.rs
@@ -1950,10 +1950,9 @@ impl RawEngine {
                     "JSON_ARRAYAGG" | "JSON_OBJECTAGG" => MysqlColumnType::Blob,
                     "JSON_ARRAY" => MysqlColumnType::VarChar,
                     "JSON_ARRAY_APPEND" | "JSON_ARRAY_INSERT" => MysqlColumnType::LongBlob,
-                    "JSON_MERGE_PRESERVE" => MysqlColumnType::LongBlob,
+                    "JSON_MERGE" | "JSON_MERGE_PRESERVE" => MysqlColumnType::LongBlob,
                     "JSON_EXTRACT"
                     | "JSON_INSERT"
-                    | "JSON_MERGE"
                     | "JSON_MERGE_PATCH"
                     | "JSON_OBJECT"
                     | "JSON_REMOVE"
